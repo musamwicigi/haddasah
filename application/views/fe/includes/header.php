a7a6b206651d820c78fbdc72a4cc9335a380e971
mwicigi <mwicigi@hypertechsolutions.co.ke>
2017-09-07 19:21:18 +0300

    update the header with such at top

--- application/views/fe/includes/header.php
+++ application/views/fe/includes/header.php
@@ -29,164 +29,6 @@
       <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
   <![endif]--> 
 
-<!-- mobile menu -->
-<div id="mobile-menu">
-  <div id="mobile-search">
-    <form>
-      <div class="input-group">
-        <input type="text" class="form-control" placeholder="Search" name="search">
-        <button class="btn-search" type="button"><i class="fa fa-search"></i></button>
-      </div>
-    </form>
-  </div>
-  <ul>
-    <li><a href="index.html" class="home1">Home</a></li>
-    <li><a href="shop_grid.html">Women</a></li>
-    <li><a href="contact_us.html">Men</a></li>
-    <li><a href="about_us.html">Kids</a></li>
-    <li><a href="blog.html">Other Crafts</a></li>
-    <li><a href="index.html">About US</a></li>
-    <li><a href="shop_grid.html">Blog</a></li>
-    <li><a href="contact_us.html">Contact us</a></li>
-  </ul>  
-</div>
-<!-- end mobile menu -->
-<div id="page"> 
-  
-  <!-- Header -->
-  <header>
-    <div class="header-container">
-      <div class="header-top">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-4 col-sm-4 hidden-xs"> 
-              <!-- Default Welcome Message -->
-              <div class="welcome-msg "></div>
-            </div>
-            
-            <!-- top links -->
-            <div class="headerlinkmenu col-lg-8 col-md-7 col-sm-8 col-xs-12">
-              <div class="links">
-              
-               <div class="item-cur"> <i class="fa fa-unlock-alt"></i> <span>Login </span> </div>
-                <div class="myaccount"><a title="My Account" href="account_page.html"><i class="fa fa-user"></i><span class="hidden-xs">User Account</span></a></div>
-                
-                
-              </div>
-              <div class="language-currency-wrapper">
-                <div class="inner-cl">
-                  <div class="block block-language form-language" style="text-align: center;">
-                    <div class="lg-cur"> <span> <span class="lg-fr"></span> <i class="fa fa-facebook"></i> </span> </div>
-                    
-                  </div>
-                  <div class="block block-currency" style="text-align: center;">
-                    <div class="item-cur"> <span></span> <i class="fa fa-twitter"></i></div>
-                    
-                  </div>
-                  <div class="block block-currency" style="text-align: center;">
-                    <div class="item-cur"> <span></span> <i class="fa fa-instagram"></i></div>
-                    
-                  </div>
-                </div>
-                
-                <!-- End Default Welcome Message --> 
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="container" style="background-color: black">
-        <div class="row">
-          <div class="col-sm-4 col-md-3 col-xs-8"> 
-            <!-- Header Logo -->
-            <div class="logo"><a title="e-commerce" href="index.html"><img alt="e-commerce" src="<?php echo base_url();?>assets/images/logo.png" width="140"></a> </div>
-            <!-- End Header Logo --> 
-          </div>
-          <div class="col-md-9 col-sm-8 col-xs-4">
-            <div class="mtmegamenu">
-              <ul class="hidden-xs">
-                <li class="mt-root demo_custom_link_cms">
-                  <div class="mt-root-item"><a href="index.html">
-                    <div class="title title_font"><span class="title-text" style="color: white">Home</span></div>
-                    </a></div>
-                  
-                </li>
-               
-                   <li class="mt-root">
-                  <div class="mt-root-item"><a href="index.html#">
-                    <div class="title title_font"><span class="title-text" style="color: white">Women</span></div>
-                    </a>
-                  </div>
-                  </li>
-                   <li class="mt-root">
-                  <div class="mt-root-item"><a href="index.html#">
-                    <div class="title title_font"><span class="title-text" style="color: white">Men</span></div>
-                    </a>
-                  </div>
-                  </li>
-                   <li class="mt-root">
-                  <div class="mt-root-item"><a href="index.html#">
-                    <div class="title title_font"><span class="title-text" style="color: white">Kids</span></div>
-                    </a>
-                  </div>
-                  </li>
-                   <li class="mt-root">
-                  <div class="mt-root-item"><a href="index.html#">
-                    <div class="title title_font"><span class="title-text" style="color: white">Other Crafts</span></div>
-                    </a>
-                  </div>
-                  </li>
-                  
-
-                </ul>
-                 
-              <!-- top cart -->
-              <div class="col-md-3 col-xs-9 col-sm-2 top-cart" style="text-align: center;">
-                <div class="top-cart-contain">
-                  <div class="mini-cart">
-                    <div data-toggle="dropdown" data-hover="dropdown" class="basket dropdown-toggle"> <a href="index.html#">
-                      <div class="cart-icon"><i class="fa fa-shopping-cart" style="padding:10px;"></i></div>
-                      <div class="shoppingcart-inner hidden-xs hidden-sm"><span class="cart-title" style="color: white">Shopping Cart</span> <span class="cart-total" style="color: white">3 Item(s): Kes 5200.00</span></div>
-                      </a></div>
-                    <div>
-                      <div class="top-cart-content">
-                        <div class="block-subtitle hidden-xs">Recently added item(s)</div>
-                        <ul id="cart-sidebar" class="mini-products-list">
-                          <li class="item odd"> <a href="shopping_cart.html" title="Ipsums Dolors Untra" class="product-image"><img src="<?php echo base_url();?>assets/images/products/product2.jpg" alt="Lorem ipsum dolor" width="65"></a>
-                            <div class="product-details"> <a href="index.html#" title="Remove This Item" class="remove-cart"><i class="icon-close"></i></a>
-                              <p class="product-name"><a href="shopping_cart.html">Hand made necklace</a> </p>
-                              <strong>4</strong> x <span class="price">Kes 80.00</span> </div>
-                          </li>
-                          <li class="item even"> <a href="shopping_cart.html" title="Ipsums Dolors Untra" class="product-image"><img src="<?php echo base_url();?>assets/images/products/product3.jpg" alt="Lorem ipsum dolor" width="65"></a>
-                            <div class="product-details"> <a href="index.html#" title="Remove This Item" class="remove-cart"><i class="icon-close"></i></a>
-                               <p class="product-name"><a href="shopping_cart.html">Hand made necklace</a> </p>
-                              <strong>4</strong> x <span class="price">Kes 80.00</span> </div>
-                          </li>
-                          <li class="item last odd"> <a href="shopping_cart.html" title="Ipsums Dolors Untra" class="product-image"><img src="<?php echo base_url();?>assets/images/products/product4.jpg" alt="Lorem ipsum dolor" width="65"></a>
-                            <div class="product-details"> <a href="index.html#" title="Remove This Item" class="remove-cart"><i class="icon-close"></i></a>
-                               <p class="product-name"><a href="shopping_cart.html">Hand made necklace</a> </p>
-                              <strong>4</strong> x <span class="price">Kes 80.00</span> </div>
-                          </li>
-                        </ul>
-                        <div class="top-subtotal">Subtotal: <span class="price"> Kes 2520.00</span></div>
-                        <div class="actions">
-                          <button class="btn-checkout" type="button"><i class="fa fa-check"></i><span>Checkout</span></button>
-                          <button class="view-cart" type="button"><i class="fa fa-shopping-cart"></i> <span>View Cart</span></button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-          
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </header>
-  <!-- end header --> 
-  
   <!-- Navbar -->
   <nav>
     <div class="container">
@@ -401,11 +243,141 @@
           
           <!-- End Search --> 
         </div>
-        <div class="call-us hidden-sm hidden-xs"> <i class="fa fa-phone"></i>
-          <div class="call-us-inner"> <span class="call-text">free call us</span> <span class="call-num">Call: + 0123 456 789</span> </div>
+        <div class="hidden-sm hidden-xs col-md-3"> 
+           <div class="language-currency-wrapper">
+
+                <div class="inner-cl">
+                  <div class="block block-language form-language" style="text-align: center;">
+                    <div class="lg-cur"> <span> <span class="lg-fr"></span> <i class="fa fa-facebook circle"></i> </span> </div>
+                  </div>
+                  <div class="block block-currency" style="text-align: center;">
+                    <div class="item-cur"> <span></span> <i class="fa fa-twitter circle"></i></div>
+                  </div>
+                  <div class="block block-currency" style="text-align: center;">
+                    <div class="item-cur"> <span></span> <i class="fa fa-instagram circle"></i></div>  
+                  </div>
+                </div>
+                
+                <!-- End Default Welcome Message --> 
+              </div>
         </div>
       </div>
     </div>
   </nav>
-  <!-- end nav --> 
+ 
+<!-- mobile menu -->
+<div id="mobile-menu">
+  <div id="mobile-search">
+    <form>
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Search" name="search">
+        <button class="btn-search" type="button"><i class="fa fa-search"></i></button>
+      </div>
+    </form>
+  </div>
+  <ul>
+    <li><a href="index.html" class="home1">Home</a></li>
+    <li><a href="shop_grid.html">Women</a></li>
+    <li><a href="contact_us.html">Men</a></li>
+    <li><a href="about_us.html">Kids</a></li>
+    <li><a href="blog.html">Other Crafts</a></li>
+    <li><a href="index.html">About US</a></li>
+    <li><a href="shop_grid.html">Blog</a></li>
+    <li><a href="contact_us.html">Contact us</a></li>
+  </ul>  
+</div>
+
+
+      <div class="container" style="background-color: black">
+        <div class="row">
+          <div class="col-sm-4 col-md-3 col-xs-8"> 
+            <!-- Header Logo -->
+            <div class="logo"><a title="e-commerce" href="index.html"><img alt="e-commerce" src="<?php echo base_url();?>assets/images/logo.png" width="140"></a> </div>
+            <!-- End Header Logo --> 
+          </div>
+          <div class="col-md-9 col-sm-8 col-xs-4">
+            <div class="mtmegamenu">
+              <ul class="hidden-xs">
+                <li class="mt-root demo_custom_link_cms">
+                  <div class="mt-root-item"><a href="index.html">
+                    <div class="title title_font"><span class="title-text" style="color: white">Home</span></div>
+                    </a></div>
+                  
+                </li>
+               
+                   <li class="mt-root">
+                  <div class="mt-root-item"><a href="index.html#">
+                    <div class="title title_font"><span class="title-text" style="color: white">Women</span></div>
+                    </a>
+                  </div>
+                  </li>
+                   <li class="mt-root">
+                  <div class="mt-root-item"><a href="index.html#">
+                    <div class="title title_font"><span class="title-text" style="color: white">Men</span></div>
+                    </a>
+                  </div>
+                  </li>
+                   <li class="mt-root">
+                  <div class="mt-root-item"><a href="index.html#">
+                    <div class="title title_font"><span class="title-text" style="color: white">Kids</span></div>
+                    </a>
+                  </div>
+                  </li>
+                   <li class="mt-root">
+                  <div class="mt-root-item"><a href="index.html#">
+                    <div class="title title_font"><span class="title-text" style="color: white">Other Crafts</span></div>
+                    </a>
+                  </div>
+                  </li>
+                  
+
+                </ul>
+                 
+              <!-- top cart -->
+              <div class="col-md-3 col-xs-9 col-sm-2 top-cart" style="text-align: center;">
+                <div class="top-cart-contain">
+                  <div class="mini-cart">
+                    <div data-toggle="dropdown" data-hover="dropdown" class="basket dropdown-toggle"> <a href="index.html#">
+                      <div class="cart-icon"><i class="fa fa-shopping-cart" style="padding:10px;"></i></div>
+                      <div class="shoppingcart-inner hidden-xs hidden-sm"><span class="cart-title" style="color: white">Shopping Cart</span> <span class="cart-total" style="color: white">3 Item(s): Kes 5200.00</span></div>
+                      </a></div>
+                    <div>
+                      <div class="top-cart-content">
+                        <div class="block-subtitle hidden-xs">Recently added item(s)</div>
+                        <ul id="cart-sidebar" class="mini-products-list">
+                          <li class="item odd"> <a href="shopping_cart.html" title="Ipsums Dolors Untra" class="product-image"><img src="<?php echo base_url();?>assets/images/products/product2.jpg" alt="Lorem ipsum dolor" width="65"></a>
+                            <div class="product-details"> <a href="index.html#" title="Remove This Item" class="remove-cart"><i class="icon-close"></i></a>
+                              <p class="product-name"><a href="shopping_cart.html">Hand made necklace</a> </p>
+                              <strong>4</strong> x <span class="price">Kes 80.00</span> </div>
+                          </li>
+                          <li class="item even"> <a href="shopping_cart.html" title="Ipsums Dolors Untra" class="product-image"><img src="<?php echo base_url();?>assets/images/products/product3.jpg" alt="Lorem ipsum dolor" width="65"></a>
+                            <div class="product-details"> <a href="index.html#" title="Remove This Item" class="remove-cart"><i class="icon-close"></i></a>
+                               <p class="product-name"><a href="shopping_cart.html">Hand made necklace</a> </p>
+                              <strong>4</strong> x <span class="price">Kes 80.00</span> </div>
+                          </li>
+                          <li class="item last odd"> <a href="shopping_cart.html" title="Ipsums Dolors Untra" class="product-image"><img src="<?php echo base_url();?>assets/images/products/product4.jpg" alt="Lorem ipsum dolor" width="65"></a>
+                            <div class="product-details"> <a href="index.html#" title="Remove This Item" class="remove-cart"><i class="icon-close"></i></a>
+                               <p class="product-name"><a href="shopping_cart.html">Hand made necklace</a> </p>
+                              <strong>4</strong> x <span class="price">Kes 80.00</span> </div>
+                          </li>
+                        </ul>
+                        <div class="top-subtotal">Subtotal: <span class="price"> Kes 2520.00</span></div>
+                        <div class="actions">
+                          <button class="btn-checkout" type="button"><i class="fa fa-check"></i><span>Checkout</span></button>
+                          <button class="view-cart" type="button"><i class="fa fa-shopping-cart"></i> <span>View Cart</span></button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- end header --> 
+   
   
